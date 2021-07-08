ab1d31b380ff5aff4cb5183548ca25c5e673eecc
sapphire-al2o3 <t_telkun@hotmail.com>
2021-07-08 21:44:12 +0900

    add Mono.Cecil

--- ConstFieldRemover/Program.cs
+++ ConstFieldRemover/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Mono.Cecil;
+using System.IO;
 
 namespace ConstFieldRemover
 {
@@ -10,6 +8,52 @@ namespace ConstFieldRemover
 	{
 		static void Main(string[] args)
 		{
+			if (args.Length < 1)
+			{
+				return;
+			}
+
+			string path = args[0];
+			string output = null;
+
+			if (args.Length > 1)
+			{
+				output = args[1];
+			}
+
+			using (var fs = File.Open(path, FileMode.Open, FileAccess.ReadWrite))
+			using (var ad = AssemblyDefinition.ReadAssembly(path))
+			{
+				List<FieldDefinition> fields = new List<FieldDefinition>();
+
+				foreach (var mod in ad.Modules)
+				{
+					foreach (var t in mod.Types)
+					{
+						fields.Clear();
+						foreach (var f in t.Fields)
+						{
+							if (f.HasConstant)
+							{
+								fields.Add(f);
+							}
+						}
+						foreach (var f in fields)
+						{
+							t.Fields.Remove(f);
+						}
+					}
+				}
+
+				if (output == null)
+				{
+					ad.Write();
+				}
+				else
+				{
+					ad.Write(output);
+				}
+			}
 		}
 	}
 }
