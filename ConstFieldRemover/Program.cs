fc685668b7bb91a2d8b9e7e557598d487d734d85
sapphire-al2o3 <t_telkun@hotmail.com>
2021-07-08 21:47:47 +0900

    covert indent

--- ConstFieldRemover/Program.cs
+++ ConstFieldRemover/Program.cs
@@ -4,56 +4,56 @@ using System.IO;
 
 namespace ConstFieldRemover
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			if (args.Length < 1)
-			{
-				return;
-			}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                return;
+            }
 
-			string path = args[0];
-			string output = null;
+            string path = args[0];
+            string output = null;
 
-			if (args.Length > 1)
-			{
-				output = args[1];
-			}
+            if (args.Length > 1)
+            {
+                output = args[1];
+            }
 
-			using (var fs = File.Open(path, FileMode.Open, FileAccess.ReadWrite))
-			using (var ad = AssemblyDefinition.ReadAssembly(path))
-			{
-				List<FieldDefinition> fields = new List<FieldDefinition>();
+            using (var fs = File.Open(path, FileMode.Open, FileAccess.ReadWrite))
+            using (var ad = AssemblyDefinition.ReadAssembly(path))
+            {
+                List<FieldDefinition> fields = new List<FieldDefinition>();
 
-				foreach (var mod in ad.Modules)
-				{
-					foreach (var t in mod.Types)
-					{
-						fields.Clear();
-						foreach (var f in t.Fields)
-						{
-							if (f.HasConstant)
-							{
-								fields.Add(f);
-							}
-						}
-						foreach (var f in fields)
-						{
-							t.Fields.Remove(f);
-						}
-					}
-				}
+                foreach (var mod in ad.Modules)
+                {
+                    foreach (var t in mod.Types)
+                    {
+                        fields.Clear();
+                        foreach (var f in t.Fields)
+                        {
+                            if (f.HasConstant)
+                            {
+                                fields.Add(f);
+                            }
+                        }
+                        foreach (var f in fields)
+                        {
+                            t.Fields.Remove(f);
+                        }
+                    }
+                }
 
-				if (output == null)
-				{
-					ad.Write();
-				}
-				else
-				{
-					ad.Write(output);
-				}
-			}
-		}
-	}
+                if (output == null)
+                {
+                    ad.Write();
+                }
+                else
+                {
+                    ad.Write(output);
+                }
+            }
+        }
+    }
 }
